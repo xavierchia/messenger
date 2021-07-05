6bb5ccbdb3a3f018a5a115d8dfe32a67c9a1e2b0
xavierchia <xavier.cpy@gmail.com>
2021-07-05 11:26:39 +0800

    Initial Commit

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by xavier chia on 5/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
