074a07d9fdd5e8096d19c61a5af8d28b9f25e740
xavierchia <xavier.cpy@gmail.com>
2021-07-05 11:49:56 +0800

    set up view controllers

---
+++ Messenger/Controllers/NewConversationViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NewConversationViewController.swift
+//  Messenger
+//
+//  Created by xavier chia on 5/7/21.
+//
+
+import UIKit
+
+class NewConversationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
